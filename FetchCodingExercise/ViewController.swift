6f9ae812baa5709f39e6d2b65976e80070316754
MarquisSimmons <msimmons0009@gmail.com>
2021-02-06 07:18:54 -0500

    Initial Commit

---
+++ FetchCodingExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchCodingExercise
+//
+//  Created by Marquis Simmons on 2/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
